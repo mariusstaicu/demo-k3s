495e97836729ceef3adc1be1001eed324c28c777
Marius Staicu <marius.staicu@esolutions.ro>
2020-01-13 10:24:16 +0200

    - added rest of missing files

---
+++ src/main/java/com/example/demok3s/DemoK3sApplication.java
@@ -0,0 +1,13 @@
+package com.example.demok3s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoK3sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoK3sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demok3s/DemoK3sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demok3s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoK3sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
